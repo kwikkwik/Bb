29a2f34561bc40ade2bf9e240247651c68772d5d
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-07-24 10:22:23 +0700

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -19,7 +19,7 @@ const queue = new Map();
 
 client.on("ready", () => {
     function randomStatus() {
-        let status = [`${client.guilds.size} guilds.`, `${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users.`, "my!help | Beta", "Music with my users."]
+        let status = [`${client.guilds.size} guilds.`, `${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users.`, "b!help | ${client.ping}ms"]
           let rstatus = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[rstatus], {type: 'LISTENING'});
 	}; setInterval(randomStatus, 30000)
