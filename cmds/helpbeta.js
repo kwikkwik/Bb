74c62cea796aaf8fbf346cf1ec3bb26713b451f1
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-07-25 20:17:46 +0700

    Update helpbeta.js

--- cmds/helpbeta.js
+++ cmds/helpbeta.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  let pages = ['**Page: 1** | **Core** \n\n`help`\n`helpbeta`\n`ping`\n`stats`\n`invite`\n`info`', 
+  let pages = ['**Page: 1** | **Core** \n\n`help`\n`ping`\n`stats`\n`invite`\n`info`', 
 	       '**Page: 2** | **Music** \n\n`play`\n`skip`\n`stop`\n`queue`\n`pause`\n`resume`\n`volume`\n`np`', 
          '**Page: 3** | **Fun** \n\n`8ball`\n`say`\n`sayd`\n`sayembed`\n`randommeme`',
 	 '**Page: 4** | **Utility** \n\n`userinfo`\n`serverinfo`\n`avatar`\n`weather`\n`discrim`'];
   let page = 1; // We will define what page we are on here, the default page will be 1. (You can change the default page)
  
   const embed = new Discord.RichEmbed() // Define a new embed, if you are on the `stable` branch it will be new Discord.RichEmbed()
-    .setColor(0xff2f2f) // You can set your color here
+    .setColor("GREEN") // You can set your color here
     .setTitle("Bolt Command List")
-    .setFooter(`Page ${page} of ${pages.length} • Plastik | Beta Help`) // This is the default value, showing the default page and the amount of pages in the array.
+    .setFooter(`Page ${page} of ${pages.length} • Bolt | Beta Help`) // This is the default value, showing the default page and the amount of pages in the array.
     .setDescription(pages[page-1]) // This sets the description as the default page (we are subtracting 1 since arrays start at 0)
  
   message.channel.send(embed).then(msg => { // Now, we will send the embed and pass the new msg object
@@ -29,9 +29,9 @@ exports.run = (client, message, args) => {
       backwards.on('collect', r => { // This runs when the backwards reaction is found
         if (page === 1) return; // We want to make sure if they are on the first page, they cant go back a page.
         page--; // If it can go back, push back the page number
-	embed.setTitle("Plastik Command List")
+	embed.setTitle("Bolt Command List")
         embed.setDescription(pages[page-1]); // Just like setting the first one, reset the Description to the new page
-        embed.setFooter(`Page ${page} of ${pages.length} • Plastik | Beta Help`); // This also sets the footer to view the current pagenumber
+        embed.setFooter(`Page ${page} of ${pages.length} • Bolt | Beta Help`); // This also sets the footer to view the current pagenumber
         msg.edit(embed) // Then, we can push the edit to the message
       })
      
@@ -40,7 +40,7 @@ exports.run = (client, message, args) => {
         page++; // If it can go forwards, push forwards the page number
 	embed.setTitle("Bolt Command List")
         embed.setDescription(pages[page-1]); // Just like setting the first one, reset the Description to the new page
-        embed.setFooter(`Page ${page} of ${pages.length} • Plastik | Beta Help`); // This also sets the footer to view the current pagenumber
+        embed.setFooter(`Page ${page} of ${pages.length} • Bolt | Beta Help`); // This also sets the footer to view the current pagenumber
         msg.edit(embed) // Then, we can push the edit to the message
       })
    
