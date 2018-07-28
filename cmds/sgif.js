5608934bc6d802acbdf7b363f1f2b3c00eb76e28
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-07-28 22:02:33 +0700

    Update sgif.js

--- cmds/sgif.js
+++ cmds/sgif.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const gifSearch = require("gif-search");
-const PREFIX = require("../config.json")
+const PREFIX = "b!";
 
 exports.run = (bot, message, args) => {
   if (message.author.bot) return;
