a682cb323a1259f24b4e26c938910c1a5883990c
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-07-31 20:32:43 +0700

    Update gif.js

--- cmds/gif.js
+++ cmds/gif.js
@@ -1,42 +1,30 @@
-const emb = require('discord.js').RichEmbed;
+const Discord = require("discord.js");
+const gifSearch = require("gif-search");
+const prefix = require("../config.json")
 
-exports.run = async (client, msg, args) => {
-	if(args.length < 1) return args.missing(msg, 'No query provided', this.help);
-	const searchMessage = await msg.channel.send('🔎Searching');
-	msg.channel.startTyping();
-	try{
-		const { data } = await client.req({
-			url: 'http://api.giphy.com/v1/gifs/search',
-			qs: {
-				q: encodeURIComponent(args.join(' ')),
-				api_key: 'dc6zaTOxFJmzC',
-				rating: msg.channel.nsfw ? 'r' : 'pg'
-			},
-      json: true
-		});
-		
-    if(data.length < 1) {
-      searchMessage.edit('❗**No result found**');
-      msg.channel.stopTyping();
-    }
-		const embed = new emb()
-		.setColor(client.color)
-		.setImage(data[Math.floor(Math.random()*data.length)-1].images.original.url);
-		searchMessage.edit(embed).then(()=> msg.channel.stopTyping());
-	}catch(e){
-		msg.channel.stopTyping();
-	}
-};
 
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['gif'],
-  permLevel: 0
-};
+module.exports.run = (bot, message, args, ops) => {
+ 
 
-exports.help = {
-  name : "giphy",
-  description: "Search image in ghipy with give query",
-  usage: "giphy <query>"
-};
+    if (!args[0]) return message.channel.send("`${config.prefix}gif <gif name>`");
+
+    gifSearch.query(args[0]).then(
+        gifUrl => {
+        let randomcolor = ((1 << 24) * Math.random() | 0).toString(16) //Optional
+        var embed = new Discord.RichEmbed()
+            .setColor(`#${randomcolor}`)
+            .setTimestamp() 
+            .setImage(gifUrl)
+            .setFooter(`Requested by: ${message.author.tag}`);
+        message.channel.send(embed);
+    });
+
+    
+
+}
+
+module.exports.help = { 
+name: "gif", 
+description: "", 
+usage: ""
+} 
