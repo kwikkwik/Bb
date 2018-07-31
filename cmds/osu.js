219a5c1bbab7b17951208698e891be73b80d48f2
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-07-31 19:24:21 +0700

    Update osu.js

--- cmds/osu.js
+++ cmds/osu.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (client, msg, args = []) => {
   if(args.length < 1) return args.missing(msg, 'No username provided', this.help);
   const embed = new Discord.RichEmbed()
-  .setColor('#0071FF')
+  .setColor('GREEN')
   .setImage(`http://lemmmy.pw/osusig/sig.php?colour=hexc246f2&uname=${encodeURIComponent(args.join(' '))}&pp=1&countryrank&flagshadow&flagstroke&darkheader&darktriangles&opaqueavatar&avatarrounding=4&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
  msg.channel.send({embed}); 
 };
